f16dc3381b54362ac157cf71f56e141eca5af66d
Cody A. Ray <sites@codyaray.com>
2014-02-21 14:32:03 -0600

    works segfire => kairosdb

---
+++ src/main/java/com/brighttag/trident/kairos/App.java
@@ -0,0 +1,13 @@
+package com.brighttag.trident.kairos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/brighttag/trident/kairos/KairosState.java
@@ -0,0 +1,307 @@
+package com.brighttag.trident.kairos;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URISyntaxException;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nullable;
+
+import backtype.storm.task.IMetricsContext;
+import backtype.storm.tuple.Values;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Objects;
+import com.google.common.base.Strings;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Ordering;
+import com.google.common.primitives.Longs;
+
+import org.kairosdb.client.HttpClient;
+import org.kairosdb.client.builder.Aggregator;
+import org.kairosdb.client.builder.DataPoint;
+import org.kairosdb.client.builder.LongDataPoint;
+import org.kairosdb.client.builder.Metric;
+import org.kairosdb.client.builder.MetricBuilder;
+import org.kairosdb.client.builder.QueryBuilder;
+import org.kairosdb.client.response.Queries;
+import org.kairosdb.client.response.QueryResponse;
+import org.kairosdb.client.response.Results;
+
+import storm.trident.state.OpaqueValue;
+import storm.trident.state.State;
+import storm.trident.state.StateFactory;
+import storm.trident.state.StateType;
+import storm.trident.state.map.CachedMap;
+import storm.trident.state.map.IBackingMap;
+import storm.trident.state.map.MapState;
+import storm.trident.state.map.NonTransactionalMap;
+import storm.trident.state.map.OpaqueMap;
+import storm.trident.state.map.SnapshottableMap;
+import storm.trident.state.map.TransactionalMap;
+
+import static org.kairosdb.client.util.Preconditions.checkNotNullOrEmpty;
+
+public class KairosState<T> implements IBackingMap<T> {
+
+  private static final Map<StateType, KairosSerializer> DEFAULT_SERIALIZERS =
+      ImmutableMap.<StateType, KairosSerializer>of(
+//        StateType.NON_TRANSACTIONAL, new TransactionalKairosSerializer(),
+//        StateType.TRANSACTIONAL, new TransactionalKairosSerializer(),
+        StateType.OPAQUE, new OpaqueKairosSerializer());
+
+  public static class Options<T> implements Serializable {
+    private static final long serialVersionUID = -4631189563874399265L;
+    public int localCacheSize = 5000;
+    public String globalKey = "$__GLOBAL_KEY__$";
+    public int port = 8080;
+    public KairosSerializer<T> serializer;
+    public int kairosWriteDelay = 2 * 1000; // 2 seconds = 2 x kairos default (1s for v0.9.1-0.9.2)
+  }
+
+  public static StateFactory opaque(String host) {
+    return new Factory<OpaqueValue<Long>>(StateType.OPAQUE, host, new Options<OpaqueValue<Long>>());
+  }
+
+  public static StateFactory opaque(String host, Options<OpaqueValue<Long>> options) {
+    return new Factory<OpaqueValue<Long>>(StateType.OPAQUE, host, options);
+  }
+
+  public static class Factory<T> implements StateFactory {
+    private static final long serialVersionUID = -6288582170089084573L;
+
+    private final StateType stateType;
+    private final String host;
+    private final Options<T> options;
+    private final KairosSerializer<T> serializer;
+
+    public Factory(StateType stateType, String host, Options<T> options) {
+      this.stateType = stateType;
+      this.host = host;
+      this.options = options;
+      this.serializer = Objects.firstNonNull(options.serializer, DEFAULT_SERIALIZERS.get(stateType));
+    }
+
+    @Override
+    public State makeState(Map conf, IMetricsContext metrics, int partitionIndex, int numPartitions) {
+      KairosState cachedMap = new KairosState<T>(new HttpClient(host, options.port), serializer, options.kairosWriteDelay);
+//      CachedMap cachedMap = new CachedMap(state, options.localCacheSize);
+      MapState mapState;
+      if (stateType == StateType.NON_TRANSACTIONAL) {
+        mapState = NonTransactionalMap.build(cachedMap);
+      } else if (stateType == StateType.OPAQUE) {
+        mapState = OpaqueMap.build(cachedMap);
+      } else if (stateType == StateType.TRANSACTIONAL) {
+        mapState = TransactionalMap.build(cachedMap);
+      } else {
+        throw new RuntimeException("Unknown state type: " + stateType);
+      }
+      return new SnapshottableMap<T>(mapState, new Values(options.globalKey));
+    }
+  }
+
+  private final KairosSerializer<T> serializer;
+  private final HttpClient client;
+  private final int kairosWriteDelay;
+
+  public KairosState(HttpClient client, KairosSerializer<T> serializer, int kairosWriteDelay) {
+    this.client = client;
+    this.serializer = serializer;
+    this.kairosWriteDelay = kairosWriteDelay;
+  }
+
+  private static Date toDate(Object obj) {
+    return new Date(Long.parseLong(obj.toString()));
+  }
+
+  @Override
+  public List<T> multiGet(List<List<Object>> keys) {
+    System.out.println("GET KEYS " + keys);
+    // no keys means no date etc to query by
+    if (keys.isEmpty()) {
+      return ImmutableList.of();
+    }
+    Map<List<Date>, QueryBuilder> queryMap = Maps.newHashMap();
+    for (List<Object> key : keys) {
+      Date start = toDate(key.get(0));
+      Date end = toDate(key.get(1));
+      List<Date> bucket = ImmutableList.of(start, end);
+      QueryBuilder builder = queryMap.get(bucket);
+      if (builder == null) {
+        builder = QueryBuilder.getInstance().setStart(start).setEnd(end);
+        queryMap.put(bucket, builder);
+      }
+      builder.addMetric(toMetricName(key.subList(2, key.size())))
+          .addAggregator(new AggregatorImpl("max"));
+    }
+    List<T> values = Lists.newArrayListWithExpectedSize(keys.size());
+    try {
+      for (QueryBuilder builder : queryMap.values()) {
+        System.out.println("QUERY " + builder.build());
+        try {
+          // KairosDB has a delay buffer for writes
+          // We must wait at least this long to avoid a race condition between READ and WRITE
+          Thread.sleep(kairosWriteDelay);
+        } catch (InterruptedException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        QueryResponse response = client.query(builder);
+        System.out.println("RESPONSE " + toString(response));
+        for (Queries query : response.getQueries()) {
+          for (Results result : query.getResults()) {
+            if (!Strings.isNullOrEmpty(result.getName()) && !result.getDataPoints().isEmpty()) {
+              values.add(serializer.deserialize(result));
+            } else {
+              values.add(null);
+            }
+          }
+        }
+      }
+    } catch (URISyntaxException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return values;
+  }
+
+  @Override
+  public void multiPut(List<List<Object>> keys, List<T> vals) {
+    System.out.println("PUT KEYS " + keys + " VALS " + vals);
+    MetricBuilder builder = MetricBuilder.getInstance();
+    for (int i = 0; i < keys.size(); i++) {
+      List<Object> k = keys.get(i);
+      Metric metric = builder.addMetric(toMetricName(k.subList(2, k.size())));
+      serializer.serialize(vals.get(i), metric, toDate(k.get(0)));
+    }
+    try {
+       System.out.println("STORE " + builder.build());
+      client.pushMetrics(builder);
+    } catch (URISyntaxException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  private static class AggregatorImpl implements Aggregator {
+    private final String name;
+
+    private AggregatorImpl(String name)
+    {
+      this.name = checkNotNullOrEmpty(name);
+    }
+
+    @Override
+    public String getName()
+    {
+      return name;
+    }
+
+    @Override
+    public String toJson()
+    {
+      return "\"name\": \"" + name + "\"";
+    }
+  }
+
+  private static interface KairosSerializer<T> extends Serializable {
+    void serialize(T obj, Metric m, Date timestamp);
+    T deserialize(Results r);
+  }
+
+  private static class OpaqueKairosSerializer implements KairosSerializer<OpaqueValue<Long>> {
+    private static final long serialVersionUID = 2284757678780545341L;
+
+    @Override
+    public void serialize(OpaqueValue<Long> obj, Metric metric, Date timestamp) {
+      metric.addTag("currTxid", String.valueOf(obj.getCurrTxid()));
+      metric.addTag("prev", String.valueOf(obj.getPrev()));
+      metric.addDataPoint(timestamp.getTime(), obj.getCurr());
+    }
+
+    @Override
+    public OpaqueValue<Long> deserialize(Results r) {
+      Map<String, List<String>> tags = r.getTags();
+      long transactionId = Long.parseLong(NULLABLE_STRING_LONG_ORDER.max(tags.get("currTxid")));
+      String s = NULLABLE_STRING_LONG_ORDER.max(tags.get("prev"));
+      Long prev = s.equals("null") ? null : Long.parseLong(s);
+      long curr = ((LongDataPoint) VALUE_ORDER.max(r.getDataPoints())).getValue();
+      return new OpaqueValue<Long>(transactionId, curr, prev);
+    }
+
+    private static Ordering<DataPoint> VALUE_ORDER = new Ordering<DataPoint>() {
+      @Override
+      public int compare(DataPoint l, DataPoint r) {
+        LongDataPoint left = (LongDataPoint) l;
+        LongDataPoint right = (LongDataPoint) r;
+        return Longs.compare(left.getValue(), right.getValue());
+      }
+    };
+
+    private static Ordering<String> NULLABLE_STRING_LONG_ORDER = new Ordering<String>() {
+      @Override
+      public int compare(@Nullable String l, @Nullable String r) {
+        if (l.equals("null")) { return -1; }
+        if (r.equals("null")) { return  1; }
+        return Longs.compare(Long.parseLong(l), Long.parseLong(r));
+      }
+    };
+
+  }
+
+  private static final Joiner METRIC_NAME_JOINER = Joiner.on('.').skipNulls();
+
+  private static String toMetricName(List<Object> key) {
+    return METRIC_NAME_JOINER.join(key);
+  }
+
+  private static String toString(QueryResponse response) {
+    return FluentIterable.from(response.getQueries()).transformAndConcat(new Function<Queries,List<String>>() {
+      @Override
+      public List<String> apply(Queries input) {
+        return KairosState.toString(input);
+      }
+    }).toList().toString();
+  }
+
+  private static List<String> toString(Queries queries) {
+    return FluentIterable.from(queries.getResults()).transform(new Function<Results,String>() {
+      @Override
+      public String apply(Results input) {
+        return KairosState.toString(input);
+      }
+    }).toList();
+  }
+
+  private static String toString(Results results) {
+    return Objects.toStringHelper(results)
+        .add("name", results.getName())
+        .add("datapoints", toString(results.getDataPoints()))
+        .add("tags", results.getTags())
+        .add("groups", results.getGroupResults())
+        .toString();
+  }
+
+  private static String toString(List<DataPoint> datapoints) {
+    return FluentIterable.from(datapoints).transform(new Function<DataPoint,String>() {
+      @Override
+      public String apply(DataPoint input) {
+        LongDataPoint datapoint = (LongDataPoint) input;
+        return "[" + datapoint.getTimestamp() + "," + datapoint.getValue() + "]";
+      }
+    }).toList().toString();
+  }
+
+}
