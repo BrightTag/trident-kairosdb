be93b8cd524ebde0ec02d48e95760075da6a1225
Cody A. Ray <sites@codyaray.com>
2014-03-13 11:33:44 -0500

    Refactor to DRY KairosState and add comments

--- src/main/java/com/brighttag/trident/kairos/KairosState.java
+++ src/main/java/com/brighttag/trident/kairos/KairosState.java
@@ -22,6 +22,7 @@ import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+import org.apache.commons.lang.NotImplementedException;
 import org.kairosdb.client.HttpClient;
 import org.kairosdb.client.builder.CustomDataPoint;
 import org.kairosdb.client.builder.DataPoint;
@@ -36,6 +37,7 @@ import storm.trident.state.JSONNonTransactionalSerializer;
 import storm.trident.state.JSONOpaqueSerializer;
 import storm.trident.state.JSONTransactionalSerializer;
 import storm.trident.state.OpaqueValue;
+import storm.trident.state.Serializer;
 import storm.trident.state.State;
 import storm.trident.state.StateFactory;
 import storm.trident.state.StateType;
@@ -52,6 +54,21 @@ import storm.trident.state.map.TransactionalMap;
  * Trident state implementation for KairosDB. It supports non-transactional,
  * transactional, and opaque state types.
  *
+ * Requires use of KairosDB built with support for "storm_opaque", "storm_transactional",
+ * and "storm_nontransactional" custom types.
+ *
+ * Only {@code GroupedStream}s are supported, keyed by {@code (bucketStart, bucketEnd, name, tags)}.
+ * For example, assuming your spout emits fields {@code (bucketStart, bucketEnd, name, value, tags)},
+ * you might setup an aggregation topology as follows. (This is the only currently tested setup).
+ *
+ * <pre>{@code
+ *   TridentTopology topology = new TridentTopology();
+ *   Stream stream = topology.newStream("spout1", spout)
+ *       .groupBy(new Fields("bucketStart", "bucketEnd", "metricName", "tags"))
+ *       .persistentAggregate(KairosState.opaque(kairosHost), new Fields("value"), new Sum(), new Fields("count"))
+ *   return topology.build();
+ * }</pre>
+ *
  * @author codyaray
  * @since 2/18/2014
  * @param <T> type of the value; one of 'Long', 'TransactionalValue<Long>', or 'OpaqueValue<Long>'
@@ -59,52 +76,73 @@ import storm.trident.state.map.TransactionalMap;
 public class KairosState<T> implements IBackingMap<T> {
 
   @SuppressWarnings("rawtypes")
-  private static final Map<StateType, KairosSerializer> DEFAULT_SERIALIZERS =
-      ImmutableMap.<StateType, KairosSerializer>of(
-        StateType.NON_TRANSACTIONAL, new NonTransactionalKairosSerializer(),
-        StateType.TRANSACTIONAL, new TransactionalKairosSerializer(),
-        StateType.OPAQUE, new OpaqueKairosSerializer());
+  private static final Map<StateType, Serializer> DEFAULT_SERIALIZERS =
+      ImmutableMap.<StateType, Serializer>of(
+        StateType.NON_TRANSACTIONAL, new JSONTransactionalSerializer(),
+        StateType.TRANSACTIONAL, new JSONTransactionalSerializer(),
+        StateType.OPAQUE, new JSONOpaqueSerializer());
 
   public static class Options<T> implements Serializable {
     private static final long serialVersionUID = -4631189563874399265L;
     public int localCacheSize = 5000;
     public String globalKey = "$__GLOBAL_KEY__$";
     public int port = 8080;
-    public KairosSerializer<T> serializer;
+    public Serializer<T> serializer;
     public int kairosWriteDelay = 2 * 1000; // 2 seconds = 2 x kairos default (1s for v0.9.1-0.9.2)
   }
 
+  /**
+   * Return an opaque {@link KairosState.Factory}.
+   */
   public static StateFactory opaque(String host) {
     return opaque(host, new Options<OpaqueValue<Long>>());
   }
 
+  /**
+   * Return an opaque {@link KairosState.Factory} with the given {@code options}.
+   */
   public static StateFactory opaque(String host, Options<OpaqueValue<Long>> options) {
     return new Factory<OpaqueValue<Long>>(StateType.OPAQUE, host, options);
   }
 
+  /**
+   * Return a transactional {@link KairosState.Factory}.
+   */
   public static StateFactory transactional(String host) {
     return transactional(host, new Options<TransactionalValue<Long>>());
   }
 
+  /**
+   * Return a transactional {@link KairosState.Factory} with the given {@code options}.
+   */
   public static StateFactory transactional(String host, Options<TransactionalValue<Long>> options) {
     return new Factory<TransactionalValue<Long>>(StateType.OPAQUE, host, options);
   }
 
+  /**
+   * Return a non-transactional {@link KairosState.Factory}.
+   */
   public static StateFactory nonTransactional(String host) {
     return nonTransactional(host, new Options<Long>());
   }
 
+  /**
+   * Return a non-transactional {@link KairosState.Factory} with the given {@code options}.
+   */
   public static StateFactory nonTransactional(String host, Options<Long> options) {
     return new Factory<Long>(StateType.NON_TRANSACTIONAL, host, options);
   }
 
+  /**
+   * Factory for creating {@link KairosState}s.
+   */
   public static class Factory<T> implements StateFactory {
     private static final long serialVersionUID = -6288582170089084573L;
 
     private final StateType stateType;
     private final String host;
     private final Options<T> options;
-    private final KairosSerializer<T> serializer;
+    private final Serializer<T> serializer;
 
     @SuppressWarnings("unchecked")
     public Factory(StateType stateType, String host, Options<T> options) {
@@ -115,57 +153,47 @@ public class KairosState<T> implements IBackingMap<T> {
     }
 
     @Override
-    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @SuppressWarnings("rawtypes")
     public State makeState(Map conf, IMetricsContext metrics, int partitionIndex, int numPartitions) {
       KairosState<T> state = new KairosState<T>(new HttpClient(host, options.port), serializer, options.kairosWriteDelay);
-      CachedMap cachedMap = new CachedMap(state, options.localCacheSize);
-      MapState mapState;
-      if (stateType == StateType.NON_TRANSACTIONAL) {
-        mapState = NonTransactionalMap.build(cachedMap);
-      } else if (stateType == StateType.OPAQUE) {
-        mapState = OpaqueMap.build(cachedMap);
-      } else if (stateType == StateType.TRANSACTIONAL) {
-        mapState = TransactionalMap.build(cachedMap);
-      } else {
+      CachedMap<T> cachedMap = new CachedMap<T>(state, options.localCacheSize);
+      MapState<T> mapState = buildMapState(cachedMap);
+      return new SnapshottableMap<T>(mapState, new Values(options.globalKey));
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private MapState<T> buildMapState(CachedMap cachedMap) {
+      switch (stateType) {
+      case NON_TRANSACTIONAL:
+        return NonTransactionalMap.build(cachedMap);
+      case OPAQUE:
+        return OpaqueMap.build(cachedMap);
+      case TRANSACTIONAL:
+        return TransactionalMap.build(cachedMap);
+      default:
         throw new RuntimeException("Unknown state type: " + stateType);
       }
-      return new SnapshottableMap<T>(mapState, new Values(options.globalKey));
     }
   }
 
-  private final KairosSerializer<T> serializer;
+  private final Serializer<T> serializer;
   private final HttpClient client;
   private final int kairosWriteDelay;
 
-  public KairosState(HttpClient client, KairosSerializer<T> serializer, int kairosWriteDelay) {
+  public KairosState(HttpClient client, Serializer<T> serializer, int kairosWriteDelay) {
     this.client = client;
     this.serializer = serializer;
     this.kairosWriteDelay = kairosWriteDelay;
   }
 
   /**
-   * Parses the string form of {@code obj} as a signed decimal long representing a date.
-   * Throws an exception on failure.
+   * Fetch multiple metrics from KairosDB.
    *
-   * @return the Date representation of {@code obj}
+   * @param keys list of metric key fields in this order:
+   *   (start_date [long], exclusive_end_date [long], name [string], tags [map<string, string>]...)
+   * @return list of values corresponding to the {@code key} with the same index in {@code keys}
+   *   or {@code null} for each key without a value
    */
-  private static Date toDate(Object obj) {
-    return toDate(obj, false);
-  }
-
-  /**
-   * Parses the string form of {@code obj} as a signed decimal long representing a date.
-   * If {@code obj} is {@code exclusive} (i.e., {@code exclusive is true), subtracts one
-   * millisecond to transform from an exclusive date to an inclusive date.
-   * Throws an exception on failure.
-   *
-   * @param exclusive whether {@obj} is exclusive
-   * @return the Date representation of {@code obj}
-   */
-  private static Date toDate(Object obj, boolean exclusive) {
-    return new Date(Long.parseLong(obj.toString()) - (exclusive ? 1 : 0));
-  }
-
   @Override
   public List<T> multiGet(List<List<Object>> keys) {
     // no keys means no date etc to query by
@@ -194,7 +222,7 @@ public class KairosState<T> implements IBackingMap<T> {
         for (Queries query : response.getQueries()) {
           for (Results result : query.getResults()) {
             if (!Strings.isNullOrEmpty(result.getName()) && !result.getDataPoints().isEmpty()) {
-              values.add(serializer.deserialize(result));
+              values.add(deserialize(result));
             } else {
               values.add(null);
             }
@@ -210,13 +238,21 @@ public class KairosState<T> implements IBackingMap<T> {
     return values;
   }
 
+  /**
+   * Store multiple metrics into KairosDB. This save is idempotent; any data for
+   * {@code keys} that already exists is overwritten with the new {@code vals}.
+   *
+   * @param keys list of metric key fields in this order:
+   *   (start_date [long], exclusive_end_date [long], name [string], tags [map<string, string>]...)
+   * @return list of values corresponding to the {@code key} with the same index in {@code keys}
+   */
   @Override
   public void multiPut(List<List<Object>> keys, List<T> vals) {
     MetricBuilder builder = MetricBuilder.getInstance();
     for (int i = 0; i < keys.size(); i++) {
       List<Object> k = keys.get(i);
-      Metric metric = builder.addMetric(k.get(2).toString());
-      serializer.serialize(vals.get(i), metric, toDate(k.get(0)), toTags(k.subList(3, k.size())));
+      Metric metric = builder.addMetric(k.get(2).toString()).addTags(toTags(k.subList(3, k.size())));
+      serialize(metric, toDate(k.get(0)), vals.get(i));
     }
     try {
        System.out.println("STORE " + builder.build());
@@ -231,7 +267,52 @@ public class KairosState<T> implements IBackingMap<T> {
     }
   }
 
-  private Map<String, String> toTags(List<Object> objs) {
+  /**
+   * KairosDB has a delay buffer for writes. We must wait at least
+   * this long to avoid a race condition between READ and WRITE
+   */
+  private void waitForWrite() {
+    try {
+      Thread.sleep(kairosWriteDelay);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+  }
+
+  /**
+   * Adds a datapoint to {@code metric} consisting of the {@code timestamp}, the serialized
+   * form of {@code obj}, and the custom data type for the current {@code serializer}.
+   */
+  private void serialize(Metric metric, Date timestamp, T obj) {
+    String value = new String(serializer.serialize(obj), Charsets.UTF_8);
+    metric.addDataPoint(timestamp.getTime(), value, toType(serializer));
+  }
+
+  /**
+   * Parses a datapoint value from the KairosDB response, returning the deserialized value.
+   */
+  private T deserialize(Results r) {
+    // Guaranteed to be a single element because we query for a single bucket
+    @SuppressWarnings("unchecked")
+    String value = ((CustomDataPoint<String>) Iterables.getOnlyElement(r.getDataPoints())).getValue();
+    return serializer.deserialize(value.getBytes(Charsets.UTF_8));
+  }
+
+  @SuppressWarnings("rawtypes")
+  private static final Map<Class<? extends Serializer>, String> KAIROS_TYPES = ImmutableMap.of(
+      JSONOpaqueSerializer.class, "storm_opaque",
+      JSONTransactionalSerializer.class, "storm_transactional",
+      JSONNonTransactionalSerializer.class, "storm_nontransactional");
+
+  private static String toType(Serializer<?> serializer) {
+    if (KAIROS_TYPES.containsKey(serializer.getClass())) {
+      return KAIROS_TYPES.get(serializer.getClass());
+    }
+    throw new NotImplementedException("Kairos custom type for serializer " + serializer.getClass());
+  }
+
+  private static Map<String, String> toTags(List<Object> objs) {
     Map<String, String> tags = Maps.newHashMap();
     for (Object o : objs) {
       if (o instanceof Map) {
@@ -245,86 +326,27 @@ public class KairosState<T> implements IBackingMap<T> {
     return tags;
   }
 
-  private void waitForWrite() {
-    try {
-      // KairosDB has a delay buffer for writes
-      // We must wait at least this long to avoid a race condition between READ and WRITE
-      Thread.sleep(kairosWriteDelay);
-    } catch (InterruptedException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+  /**
+   * Parses the string form of {@code obj} as a signed decimal long representing a date.
+   * Throws an exception on failure.
+   *
+   * @return the Date representation of {@code obj}
+   */
+  private static Date toDate(Object obj) {
+    return toDate(obj, false);
   }
 
-  private static interface KairosSerializer<T> extends Serializable {
-    void serialize(T obj, Metric m, Date timestamp, Map<String, String> tags);
-    T deserialize(Results r);
-  }
-
-  private static class OpaqueKairosSerializer implements KairosSerializer<OpaqueValue<Long>> {
-    private static final long serialVersionUID = 2284757678780545341L;
-
-    private static final JSONOpaqueSerializer SERIALIZER = new JSONOpaqueSerializer();
-
-    @Override
-    public void serialize(OpaqueValue<Long> obj, Metric metric, Date timestamp, Map<String, String> tags) {
-      String value = new String(SERIALIZER.serialize(obj), Charsets.UTF_8);
-      metric.addTags(tags);
-      metric.addDataPoint(timestamp.getTime(), value, "storm_opaque");
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public OpaqueValue<Long> deserialize(Results r) {
-      // Guaranteed to be a single element because we query for a single bucket
-      String value = ((CustomDataPoint<String>) Iterables.getOnlyElement(r.getDataPoints())).getValue();
-      return SERIALIZER.deserialize(value.getBytes(Charsets.UTF_8));
-    }
-
-  }
-
-  private static class TransactionalKairosSerializer implements KairosSerializer<TransactionalValue<Long>> {
-    private static final long serialVersionUID = 2284757678780545341L;
-
-    private static final JSONTransactionalSerializer SERIALIZER = new JSONTransactionalSerializer();
-
-    @Override
-    public void serialize(TransactionalValue<Long> obj, Metric metric, Date timestamp, Map<String, String> tags) {
-      String value = new String(SERIALIZER.serialize(obj), Charsets.UTF_8);
-      metric.addTags(tags);
-      metric.addDataPoint(timestamp.getTime(), value, "storm_transactional");
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public TransactionalValue<Long> deserialize(Results r) {
-      // TODO comment - why is it guaranteed to only be a single element in the response?
-      String value = ((CustomDataPoint<String>) Iterables.getOnlyElement(r.getDataPoints())).getValue();
-      return SERIALIZER.deserialize(value.getBytes(Charsets.UTF_8));
-    }
-
-  }
-
-  private static class NonTransactionalKairosSerializer implements KairosSerializer<Long> {
-    private static final long serialVersionUID = 2284757678780545341L;
-
-    private static final JSONNonTransactionalSerializer SERIALIZER = new JSONNonTransactionalSerializer();
-
-    @Override
-    public void serialize(Long obj, Metric metric, Date timestamp, Map<String, String> tags) {
-      String value = new String(SERIALIZER.serialize(obj), Charsets.UTF_8);
-      metric.addTags(tags);
-      metric.addDataPoint(timestamp.getTime(), value, "storm_nontransactional");
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public Long deserialize(Results r) {
-      // TODO comment - why is it guaranteed to only be a single element in the response?
-      String value = ((CustomDataPoint<String>) Iterables.getOnlyElement(r.getDataPoints())).getValue();
-      return (Long)SERIALIZER.deserialize(value.getBytes(Charsets.UTF_8));
-    }
-
+  /**
+   * Parses the string form of {@code obj} as a signed decimal long representing a date.
+   * If {@code obj} is {@code exclusive} (i.e., {@code exclusive is true), subtracts one
+   * millisecond to transform from an exclusive date to an inclusive date.
+   * Throws an exception on failure.
+   *
+   * @param exclusive whether {@obj} is exclusive
+   * @return the Date representation of {@code obj}
+   */
+  private static Date toDate(Object obj, boolean exclusive) {
+    return new Date(Long.parseLong(obj.toString()) - (exclusive ? 1 : 0));
   }
 
   private static String toString(QueryResponse response) {
